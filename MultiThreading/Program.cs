01a1c8feee3dc475b473084a1e69e41e5a02eb54
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-26 10:03:18 +0530

    Add project files.

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+
+namespace MultiThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleExample example = new SimpleExample();
+            //example.Fun1();
+            //example.Fun2();
+            Thread cuTh = Thread.CurrentThread;
+            
+            Thread t = new Thread(new ThreadStart(example.Fun2));
+
+
+            Thread thread = new Thread(example.Fun1);
+            thread.Start();
+
+            Thread thread1 = new Thread(example.Fun2);
+            thread1.Start();
+        }
+    }
+}
---
+++ MultiThreading/SimpleExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreading
+{
+    internal class SimpleExample
+    {
+        public void Fun1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Fun 1 " +i);
+                Thread.Sleep(1000);
+            }
+            
+        }
+        public void Fun2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Fun 2 " +i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
